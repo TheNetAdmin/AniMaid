008e2a2feb86bc32e9c970e3545f53150786ee91
TheNetAdmin <zxwang42@gmail.com>
2022-05-14 05:09:13 +0000

    Fix script

--- scripts/data/gc.sh
+++ scripts/data/gc.sh
@@ -12,16 +12,16 @@ num_elements=$1
 curr_path="$(realpath "$(dirname "$0")")"
 
 jq \
-    ".[-${num_elements}:-1] | .[] | {title: .title, publish_time: .publish_time}" \
+    ".[length-${num_elements}:length] | .[] | {title: .title, publish_time: .publish_time}" \
     "${curr_path}/download.json"    \
     | tee "${curr_path}/gc_result.json" \
 ;
 
 function backup_then_gc {
     bash "${curr_path}/backup.sh"
-    jq "del(.[-${num_elements}:-1])"     "${curr_path}/download.json" > "${curr_path}/download-gced.json"
-    mv "${curr_path}/download.json"      "${curr_path}/download-pre-gc.json"
-    mv "${curr_path}/download-gced.json" "${curr_path}/download.json"
+    jq "del(.[length-${num_elements}:length)" "${curr_path}/download.json" > "${curr_path}/download-gced.json"
+    mv "${curr_path}/download.json"           "${curr_path}/download-pre-gc.json"
+    mv "${curr_path}/download-gced.json"      "${curr_path}/download.json"
 }
 
 echo "Do you wish to delete these elements?"
