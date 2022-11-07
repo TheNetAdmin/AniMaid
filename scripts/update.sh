6a698d6d5a0a4e75388d855adb93ad520e0c4336
TheNetAdmin <zxwang42@gmail.com>
2022-11-07 05:49:53 +0000

    Auto organizing

--- scripts/update.sh
+++ scripts/update.sh
@@ -5,3 +5,11 @@ curr_path=$(realpath $(dirname $0))
 cd "$curr_path/.."
 python3 animaid.py update -a
 python3 animaid.py download
+
+ORGANIZE_MINS=60
+if [ -n "${ORGANIZE_MINS}" ]; then
+echo "Scheduling organizing script in ${ORGANIZE_MINS} minutes"
+at now + "${ORGANIZE_MINS}" minute <<- EOF
+bash ./scripts/organize.sh
+EOF
+fi
